faf4c907a64732292cfd77a086e29e0b98b46fc5
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2019-04-08 14:40:32 +0200

    remove symbol

--- src/nevergreen.js
+++ src/nevergreen.js
@@ -1,21 +1,19 @@
 // Basic pathes (IE, Events, Array.from, Object.assign)
 import '@webcomponents/webcomponents-platform/webcomponents-platform.js';
 
-// The @webcomponents polyfills
-import '@webcomponents/template/template.js';
+// From from https://github.com/ungap/url-search-params
+import '@ungap/url-search-params';
 
 // From https://github.com/zloirock/core-js
 import 'core-js/features/promise';
+
+// From https://github.com/rousan/symbol-es6
+// import 'symbol-es6';
 
 // The @webcomponents polyfills
 import '@webcomponents/shadydom/src/shadydom.js';
 import '@webcomponents/custom-elements/src/custom-elements.js';
 import '@webcomponents/shadycss/entrypoints/scoping-shim.js';
-
-// From https://github.com/zloirock/core-js
-import 'core-js/features/symbol';
-import 'core-js/features/url';
-import 'core-js/features/url-search-params';
 
 // Fire a WebComponentsReady event
 import './fire-wc-event.js';
